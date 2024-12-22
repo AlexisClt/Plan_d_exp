f3771258b0bb2c460987e1a6d7486321a0ba44cb
Alexis Cullet <61407749+AlexisClt@users.noreply.github.com>
2024-12-22 13:48:54 +0100

    First tests

--- src/equations.py
+++ src/equations.py
@@ -1,3 +1,24 @@
 # main class
+import logging
+from collections.abc import Sequence
+
+logger = logging.getLogger(__name__)
+
+
 class Equations:
-    pass
+    """
+    Manage type of equations in order to speed up the generation of the equation
+    """
+
+    def __init__(self, indexes: Sequence[str], order: int) -> None:
+        """
+        Init of class
+        indexes are the indexes of the X_i s
+        order is the maximum order of the equation
+        """
+        if len(indexes) == 0:
+            raise ValueError("indexes is empty")
+        if order < 0:
+            raise ValueError(f"{order} : Wrong value for order")
+        if order == 0:
+            logger.warning("order is 0")
